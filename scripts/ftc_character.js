53bf30ddf313017f5cc895f0411fe2b038ac82cd
Hydroxi <kermitpew@gmail.com>
2018-10-10 20:11:31 +0200

    add iteration

--- scripts/ftc_character.js
+++ scripts/ftc_character.js
@@ -309,6 +309,9 @@ class FTCActor extends FTCEntity {
       // Flag whether the item is rollable
       item.data.rollable = owner.isOwner ? "ftc-rollable" : "";
 
+      // Flag whether the item is equipped
+      item.data.equippedCss =  item.data.equipped ? "equipped" : "";
+
       // Push to type
       inventory[item.type].items.push(item);
 
